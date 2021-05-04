67f30c66bdd18f90323cdace0dca68d98b8208ad
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-04 03:17:08 -0300

    Basec Static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+
+function Flooter() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/fernandolilo" target="_blank" rel="Fernando Silva">Fernando Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://facebock.FernandoSilva" target="_blank" rel="Fernando Silva">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Flooter;
