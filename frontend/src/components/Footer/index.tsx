723c1a7070597d73f752c23108dd517d21ec59ee
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-04 21:00:17 -0300

    erro em index footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,13 +3,12 @@
 function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
-            <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/fernandolilo" target="_blank" rel="Fernando Silva">Fernando Silva</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://facebock.FernandoSilva" target="_blank" rel="Fernando Silva">@devsuperior.ig</a></small></p>
-            </div>
-        </footer>
-
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/fernandolilo" target="_blank" rel="noreferrer">Fernando Silva</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+      </footer>
     );
 }
 export default Footer;
